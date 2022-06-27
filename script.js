d167bc04f4c2165f182b3dc36c136b0eebee1347
RobertaRizzuto <rtarizzuto@gmail.com>
2022-06-27 17:59:43 +0200

    chores:add_style

--- script.js
+++ script.js
@@ -6,15 +6,18 @@ const wrapperEl = newEl("div");
 const titleEl = newEl("h3");
 const welcomeEl = newEl("p");
 const oneOfUsEl = newEl("p");
+const notOneOfUsEl = newEl("p");
 
 wrapperEl.className = "wrapper";
 titleEl.className = "title";
 welcomeEl.className = "welcome";
 oneOfUsEl.className = "one_of_us";
+notOneOfUsEl.className = "not_one_of_us";
 
 titleEl.textContent = `Hi ${userLogIn}!`;
 welcomeEl.textContent = "this page is for you!";
 oneOfUsEl.textContent = "You are one of us now, contratulations!";
+notOneOfUsEl.textContent = "Your Username hasn't been saved in local storage yet, but we are gonna make up for that! Try to refresh this page and insert your username again";
 
 $body.append(wrapperEl, titleEl, welcomeEl);
 
@@ -22,9 +25,7 @@ $body.append(wrapperEl, titleEl, welcomeEl);
 
 try {
   if (localStorage.getItem("username") !== userLogIn) {
-    $body.append(
-      "Your Username hasn't been saved in local storage yet, but we are gonna make up for that! Try to refresh this page and insert your username again"
-    );
+    $body.append(notOneOfUsEl);
     throw error;
   } else {
     $body.append(oneOfUsEl);
