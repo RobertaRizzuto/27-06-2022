e866be10a4d6079b6431569d82bd16f458c0c456
RobertaRizzuto <rtarizzuto@gmail.com>
2022-06-27 17:20:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const userLogIn = prompt("Please insert your username");
+const newEl = (el) => document.createElement(el);
+const $body = document.body;
+
+const wrapperEl = newEl("div");
+const titleEl = newEl("h3");
+const welcomeEl = newEl("p");
+const oneOfUsEl = newEl("p");
+
+wrapperEl.className = "wrapper";
+titleEl.className = "title";
+welcomeEl.className = "welcome";
+oneOfUsEl.className = "one_of_us";
+
+titleEl.textContent = `Hi ${userLogIn}!`;
+welcomeEl.textContent = "this page is for you!";
+oneOfUsEl.textContent = "You are one of us now, contratulations!";
+
+$body.append(wrapperEl, titleEl, welcomeEl);
+
+//localStorage.setItem('username', userLogIn)
+
+try {
+  if (localStorage.getItem("username") !== userLogIn) {
+    $body.append(
+      "Your Username hasn't been saved in local storage yet, but we are gonna make up for that! Try to refresh this page and insert your username again"
+    );
+    throw error;
+  } else {
+    $body.append(oneOfUsEl);
+  }
+} catch (error) {
+  localStorage.setItem("username", userLogIn);
+}
